c1b83aa33b3e8ceac1599da6627ea42a64e552ad
timojokinen <jokinen.t@pm.me>
2025-06-11 14:51:46 +0200

    refactor: add logging functionality

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,7 +23,10 @@ export type Options = {
   keys?: {
     publicKey: string;
     privateKey: string;
-  }
+  };
+  logger?: {
+    log: (...args: any[]) => void;
+  };
 };
 
 const KID = 'mock-key-id';
@@ -31,166 +34,189 @@ const KID = 'mock-key-id';
 // Basic PEM format regex (very permissive)
 const pemRegex = /-----BEGIN ([A-Z ]+)-----\r?\n([A-Za-z0-9+/=\r\n]+)-----END \1-----/;
 
-/**
- * Create JSON Web Key Set (JWKS) for the public key
- */
-function createJwks(publicKey: crypto.KeyObject): { keys: crypto.JsonWebKey[] } {
-  const publicJwk = publicKey.export({ format: 'jwk' });
-  return {
-    keys: [{ ...publicJwk, alg: 'RS256', use: 'sig', kid: KID }],
-  };
-}
 
-/**
- * Creates crypto.KeyObject instances for public and private keys.
- * If keys are provided, they are used; otherwise, a new key pair is generated and saved to disk.
- * @param keys 
- * @returns 
- */
-function resolveKeyObjects(keys?: { publicKey: string; privateKey: string }): { publicKey: crypto.KeyObject; privateKey: crypto.KeyObject } {
-  if (!keys) {
-    return loadOrGenerateKeyPair();
+
+const oidcMockServerMiddleware = ({ issuer, tokenExpiration = 3600, users, baseClaims, scopes, keys, logger = console }: Options) => {
+  /**
+  * Create JSON Web Key Set (JWKS) for the public key
+  */
+  function createJwks(publicKey: crypto.KeyObject): { keys: crypto.JsonWebKey[] } {
+    const publicJwk = publicKey.export({ format: 'jwk' });
+    return {
+      keys: [{ ...publicJwk, alg: 'RS256', use: 'sig', kid: KID }],
+    };
   }
 
-  const publicKey = crypto.createPublicKey({
-    key: keys.publicKey,
-    format: 'pem',
-  });
-  const privateKey = crypto.createPrivateKey({
-    key: keys.privateKey,
-    format: 'pem',
-  });
-  return { publicKey, privateKey };
-}
+  /**
+   * Creates crypto.KeyObject instances for public and private keys.
+   * If keys are provided, they are used; otherwise, a new key pair is generated and saved to disk.
+   * @param keys 
+   * @returns 
+   */
+  function resolveKeyObjects(keys?: { publicKey: string; privateKey: string }): { publicKey: crypto.KeyObject; privateKey: crypto.KeyObject } {
+    if (!keys) {
+      logger.log('No keys provided in config, falling back to generating new key pair or loading from disk');
+      return loadOrGenerateKeyPair();
+    }
 
-/**
- * Generate or load from disk RSA key pair for signing tokens
- */
-function loadOrGenerateKeyPair(): { publicKey: crypto.KeyObject; privateKey: crypto.KeyObject } {
-  const dir = path.resolve(__dirname, '.keys');
-  const publicKeyPath = path.join(dir, 'public.pem');
-  const privateKeyPath = path.join(dir, 'private.pem');
-
-  // Generate new key pair if not found
-  if (!existsSync(privateKeyPath) || !existsSync(publicKeyPath)) {
-    if (!existsSync(dir)) mkdirSync(dir);
-
-    const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
-      modulusLength: 2048,
+    logger.log('Creating key objects from provided PEM strings');
+    const publicKey = crypto.createPublicKey({
+      key: keys.publicKey,
+      format: 'pem',
     });
-    writeFileSync(publicKeyPath, publicKey.export({ format: 'pem', type: 'spki' }));
-    writeFileSync(privateKeyPath, privateKey.export({ format: 'pem', type: 'pkcs8' }));
+    const privateKey = crypto.createPrivateKey({
+      key: keys.privateKey,
+      format: 'pem',
+    });
     return { publicKey, privateKey };
   }
 
-  // Load existing key pair from disk
-  return {
-    privateKey: crypto.createPrivateKey({
-      key: readFileSync(privateKeyPath, 'utf-8'),
-      format: 'pem',
-    }),
-    publicKey: crypto.createPublicKey({
-      key: readFileSync(publicKeyPath, 'utf-8'),
-      format: 'pem',
-    }),
-  };
-}
+  /**
+   * Generate or load from disk RSA key pair for signing tokens
+   */
+  function loadOrGenerateKeyPair(): { publicKey: crypto.KeyObject; privateKey: crypto.KeyObject } {
+    const dir = path.resolve(__dirname, '.keys');
+    const publicKeyPath = path.join(dir, 'public.pem');
+    const privateKeyPath = path.join(dir, 'private.pem');
 
-/**
- * Base64 URL encode a Buffer
- * @param input The input buffer to encode
- * @returns Base64 URL encoded string
- */
-function base64urlEncode(input: Buffer): string {
-  return input.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-}
+    // Generate new key pair if not found
+    if (!existsSync(privateKeyPath) || !existsSync(publicKeyPath)) {
+      logger.log('No existing key pair found on disk, generating new key pair');
+      if (!existsSync(dir)) mkdirSync(dir);
 
-/**
- * Base64 URL decode a string
- * @param input The input string to decode
- * @returns Decoded buffer
- */
-function base64urlDecode(input: string): string {
-  const padding = '='.repeat((4 - (input.length % 4)) % 4);
-  const base64 = input.replace(/-/g, '+').replace(/_/g, '/') + padding;
-  return Buffer.from(base64, 'base64').toString('utf-8');
-}
+      const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+      });
+      writeFileSync(publicKeyPath, publicKey.export({ format: 'pem', type: 'spki' }));
+      writeFileSync(privateKeyPath, privateKey.export({ format: 'pem', type: 'pkcs8' }));
+      return { publicKey, privateKey };
+    }
 
-const decodeJwt = (token: string) => {
-  const [headerEncoded, payloadEncoded] = token.split('.');
+    logger.log('Found existing key pair, loading from disk');
 
-  // Decode and parse the header
-  const header = JSON.parse(base64urlDecode(headerEncoded));
-
-  // Decode and parse the payload
-  const payload = JSON.parse(base64urlDecode(payloadEncoded));
-
-  return { header, payload };
-};
-
-/**
- * Sign a JWT token with the given payload and private key
- * @param payload
- * @param privateKey
- * @param options
- * @returns Signed JWT token as string
- */
-function signJwt(payload: object, privateKey: crypto.KeyObject, options: { algorithm: string; keyid: string }) {
-  const header = {
-    alg: options.algorithm,
-    typ: 'JWT',
-    kid: options.keyid,
-  };
-
-  const headerEncoded = base64urlEncode(Buffer.from(JSON.stringify(header)));
-  const payloadEncoded = base64urlEncode(Buffer.from(JSON.stringify(payload)));
-
-  const dataToSign = `${headerEncoded}.${payloadEncoded}`;
-  const signature = crypto.sign('sha256', Buffer.from(dataToSign), privateKey);
-
-  return `${dataToSign}.${base64urlEncode(signature)}`;
-}
-
-function validateAuthQueryParams(payload: any):
-  | false
-  | {
-    client_id: string;
-    redirect_uri: string;
-    code_challenge: string;
-    code_challenge_method: string;
-    response_type: string;
-    state: string;
-    nonce: string;
-    scope: string;
-  } {
-  const { client_id, redirect_uri, code_challenge, code_challenge_method, response_type, state, nonce, scope } =
-    payload;
-  if (
-    !client_id ||
-    !redirect_uri ||
-    !code_challenge ||
-    !code_challenge_method ||
-    !response_type ||
-    !state ||
-    !nonce ||
-    !scope
-  ) {
-    return false;
+    // Load existing key pair from disk
+    return {
+      privateKey: crypto.createPrivateKey({
+        key: readFileSync(privateKeyPath, 'utf-8'),
+        format: 'pem',
+      }),
+      publicKey: crypto.createPublicKey({
+        key: readFileSync(publicKeyPath, 'utf-8'),
+        format: 'pem',
+      }),
+    };
   }
 
-  return {
-    client_id: client_id.toString(),
-    redirect_uri: redirect_uri.toString(),
-    code_challenge: code_challenge.toString(),
-    code_challenge_method: code_challenge_method.toString(),
-    response_type: response_type.toString(),
-    state: state.toString(),
-    nonce: nonce.toString(),
-    scope: scope.toString(),
-  };
-}
+  /**
+   * Base64 URL encode a Buffer
+   * @param input The input buffer to encode
+   * @returns Base64 URL encoded string
+   */
+  function base64urlEncode(input: Buffer): string {
+    return input.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+  }
 
-const oidcMockServerMiddleware = ({ issuer, tokenExpiration = 3600, users, baseClaims, scopes, keys }: Options) => {
+  /**
+   * Base64 URL decode a string
+   * @param input The input string to decode
+   * @returns Decoded buffer
+   */
+  function base64urlDecode(input: string): string {
+    const padding = '='.repeat((4 - (input.length % 4)) % 4);
+    const base64 = input.replace(/-/g, '+').replace(/_/g, '/') + padding;
+    return Buffer.from(base64, 'base64').toString('utf-8');
+  }
+
+  /**
+   * Decode a JWT token and return its header and payload
+   * This function assumes the JWT is well-formed and does not perform signature verification.
+   * It is intended for use in a mock server context where the signature is not verified.
+   * @param token The JWT token to decode
+   * @returns The decoded header and payload
+   */
+  const decodeJwt = (token: string) => {
+    const [headerEncoded, payloadEncoded] = token.split('.');
+
+    // Decode and parse the header
+    const header = JSON.parse(base64urlDecode(headerEncoded));
+
+    // Decode and parse the payload
+    const payload = JSON.parse(base64urlDecode(payloadEncoded));
+
+    return { header, payload };
+  };
+
+  /**
+   * Sign a JWT token with the given payload and private key
+   * @param payload
+   * @param privateKey
+   * @param options
+   * @returns Signed JWT token as string
+   */
+  function signJwt(payload: object, privateKey: crypto.KeyObject, options: { algorithm: string; keyid: string }) {
+    const header = {
+      alg: options.algorithm,
+      typ: 'JWT',
+      kid: options.keyid,
+    };
+
+    const headerEncoded = base64urlEncode(Buffer.from(JSON.stringify(header)));
+    const payloadEncoded = base64urlEncode(Buffer.from(JSON.stringify(payload)));
+
+    const dataToSign = `${headerEncoded}.${payloadEncoded}`;
+    const signature = crypto.sign('sha256', Buffer.from(dataToSign), privateKey);
+
+    return `${dataToSign}.${base64urlEncode(signature)}`;
+  }
+
+  /**
+   * Validate the query parameters for the authorization request
+   * This function checks if all required parameters are present and returns them in a structured format.
+   * If any parameter is missing, it returns false.
+   * @param payload 
+   * @returns Validated query parameters or false if validation fails
+   */
+  function validateAuthQueryParams(payload: any):
+    | false
+    | {
+      client_id: string;
+      redirect_uri: string;
+      code_challenge: string;
+      code_challenge_method: string;
+      response_type: string;
+      state: string;
+      nonce: string;
+      scope: string;
+    } {
+    const { client_id, redirect_uri, code_challenge, code_challenge_method, response_type, state, nonce, scope } =
+      payload;
+    if (
+      !client_id ||
+      !redirect_uri ||
+      !code_challenge ||
+      !code_challenge_method ||
+      !response_type ||
+      !state ||
+      !nonce ||
+      !scope
+    ) {
+      return false;
+    }
+
+    return {
+      client_id: client_id.toString(),
+      redirect_uri: redirect_uri.toString(),
+      code_challenge: code_challenge.toString(),
+      code_challenge_method: code_challenge_method.toString(),
+      response_type: response_type.toString(),
+      state: state.toString(),
+      nonce: nonce.toString(),
+      scope: scope.toString(),
+    };
+  }
+
+
+  logger.log('Initializing OIDC mock server middleware');
   try {
     z.object({
       issuer: z.string().url(),
@@ -403,6 +429,8 @@ const oidcMockServerMiddleware = ({ issuer, tokenExpiration = 3600, users, baseC
   router.get('/token_keys', (_, res) => {
     res.json(jwks);
   });
+
+  logger.log('OIDC mock server middleware initialized successfully');
 
   return router;
 };
